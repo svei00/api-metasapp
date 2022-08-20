a0b45ce757545d067feaa853eaab767a70fc6767
svei00 <79055634+svei00@users.noreply.github.com>
2022-08-19 17:36:23 -0700

    added CRUD routes into file /routes/goals.js and changed name from metas to goals

--- routes/metas.js
+++ routes/metas.js
@@ -1,9 +1,64 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+let goals = [
+    {
+      "id": "1",
+      "details": "Run for 30 minutes",
+      "frequency": "Daily",
+      "events": 1,
+      "icon": "🏃",
+      "goal": 365,
+      "term": "2022-12-31",
+      "complete": 10
+  },
+
+  {
+      "id": "2",
+      "details": "Read Books",
+      "frequency": "Yearly",
+      "events": 6,
+      "icon": "📚",
+      "goal": 12,
+      "term": "2022-12-31",
+      "complete": 2
+  },
+
+  {
+      "id": "3",
+      "details": "Travel to National Parks",
+      "frequency": "Montly",
+      "events": 1,
+      "icon": "🏞️",
+      "goal": 60,
+      "term": "2024-12-31",
+      "complete": 40
+  }
+];
+
+/* GET Goals list. */
 router.get('/', function(req, res, next) {
-  res.send('Puto el que lo lea!');
+  // Was: res.send('Write something here!');
+});
+
+/* GET Goal with id */
+router.get('/:id', function (req, res, next) {
+
+});
+ 
+/* POST create a Goal*/
+router.post('/', function(req, res, next) {
+
+});
+
+/* PUT update a goal */
+router.put('/:id', function(req, res, next) {
+
+});
+
+/* DELETE goal */
+router.delete('/:id', function(req, res, next) {
+
 });
 
 module.exports = router;
